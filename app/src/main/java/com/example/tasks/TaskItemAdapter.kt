8a68a908786d6e43788812757ee19caf9db84350
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-05 17:43:25 +0400

    feat: RecyclerView with DiffUtil and Data Binding added

--- app/src/main/java/com/example/tasks/TaskItemAdapter.kt
+++ app/src/main/java/com/example/tasks/TaskItemAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tasks.databinding.TaskItemBinding
 
 class TaskItemAdapter
     : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
@@ -19,22 +20,19 @@ class TaskItemAdapter
         holder.bind(item)
     }
 
-    class TaskItemViewHolder(val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
-        val taskName = rootView.findViewById<TextView>(R.id.task_name)
-        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+    class TaskItemViewHolder(val binding: TaskItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
 
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.task_item, parent, false) as CardView
-                return TaskItemViewHolder(view)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return TaskItemViewHolder(binding)
             }
         }
 
         fun bind(item: Task) {
-            taskName.text = item.taskName
-            taskDone.isChecked = item.taskDone
+            binding.task = item
         }
     }
 }
