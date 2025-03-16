930237cb3e1625349f2010d16f9aa8b68a176af1
xenus <xenus.rus@gmail.com>
2025-03-16 20:48:17 +0400

    update: Upgrade Kotlin version from 1.9.24 to 2.1.10 Upgrade KSP(Kotlin Symbol Processing) from 1.9.24-1.0.20 to 2.1.10-1.0.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
 }
 
 buildscript {
